c4a2aedca985f623e09db7f847beacc454f63add
vitalii <fedvetal2012@gmail.com>
2026-04-13 01:42:11 +0300

    STP-5.2: make jsx to client transform

--- client.js
+++ client.js
@@ -1,12 +1,38 @@
-document.addEventListener('click', async(e) => {
-    if (e.target.tagName === 'A') {
-        e.preventDefault();
-        await navigate(e.target.href);
-    }
-});
+let currentPathname = window.location.pathname;
 
-async function navigate (pathname) {
-    const result = await fetch(pathname);
-    const text = await result.text();
-    document.body.innerHTML = text;
+document.addEventListener(
+    'click',
+    async(e) => {
+        if (e.target.tagName !== "A") {
+            return;
+        }
+        if (e.target.tagName === 'A') {
+            e.preventDefault();
+            await navigate(e.target.href);
+        }
+        if (e.metaKey || e.ctrlKey || e.shiftKey || e.altKey) {
+            return;
+        }
+        const href = e.target.getAttribute("href");
+        if (!href.startsWith("/")) {
+            return;
+        }
+        e.preventDefault();
+        window.history.pushState(null, null, href);
+        navigate(href);
+    },
+    true
+);
+
+async function navigate(pathname) {
+    currentPathname = pathname;
+    const response = await fetch(pathname + "?jsx");
+    const jsonString = await response.text();
+    if (pathname === currentPathname) {
+        alert(jsonString);
+    }
 }
+
+window.addEventListener("popstate", () => {
+    navigate(window.location.pathname);
+});
